656776e4dd2f7fed6f4a15ff8de2e7f11fd923f3
Chris L Chapman <ChristopherLChapman42@gmail.com>
2019-02-06 12:06:40 -0800

    switch to django-payments instead of django-paypal

--- web/hepcat/urls.py
+++ web/hepcat/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('user/', include('django.contrib.auth.urls')),
     path('user/', include('users.urls')),
     path('classes/', include('classwork.urls')),
+    path('payments/', include('payments.urls')),
 ]
 
 # Thanks to django.contrib.auth.urls The following paths are set:
