52d861052bb76881699786c9802742d7ad4320f9
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-23 21:05:32 -0800

    createsu command - fix the username setting, better exception catching.

--- web/hepcat/management/commands/createsu.py
+++ web/hepcat/management/commands/createsu.py
@@ -16,12 +16,16 @@ class Command(BaseCommand):
             self.stdout.write("This is the FIRST SuperUser. ")
         else:
             self.stdout.write("At least one SuperUser already existed. ")
-        username = os.environ.get('SUPERUSER_NAME', settings.ADMINS[0][0])
+        # username = os.environ.get('SUPERUSER_NAME', settings.ADMINS[0][0])
         email = os.environ.get('SUPERUSER_EMAIL', settings.ADMINS[0][1])
+        username = email.casefold()
         password = os.environ.get('SUPERUSER_PASS', None)
         self.stdout.write(f"username: {username}, email: {email}, pw: {password}")
         if not User.objects.filter(email=email).exists():
-            user = User.objects.create_superuser(username, email, password)
-            self.stdout.write(user)
+            try:
+                user = User.objects.create_superuser(username, email, password)
+                self.stdout.write(user)
+            except Exception as e:
+                self.stderr.write(f"Error: {e} ")
         else:
             self.stdout.write("A user already exists with that email address. ")
