47a9d359757efcb1095e6b77207d3d0ed1744e98
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-23 20:46:40 -0800

    Command should give stdout write if it was successful

--- web/hepcat/management/commands/createsu.py
+++ web/hepcat/management/commands/createsu.py
@@ -10,18 +10,18 @@ class Command(BaseCommand):
     """ Allows the creation of a superuser with a password set in the environment variables. """
 
     def handle(self, *args, **options):
-        self.stderr.write("================================================")
-        self.stderr.write(f"========== Make SUPERUSER =====================")
+        self.stdout.write("================================================")
+        self.stdout.write(f"========== Make SUPERUSER =====================")
         if not User.objects.filter(is_superuser="t").exists():
-            self.stderr.write("This is the FIRST SuperUser. ")
+            self.stdout.write("This is the FIRST SuperUser. ")
         else:
-            self.stderr.write("At least one SuperUser already existed. ")
+            self.stdout.write("At least one SuperUser already existed. ")
         username = os.environ.get('SUPERUSER_NAME', settings.ADMINS[0][0])
         email = os.environ.get('SUPERUSER_EMAIL', settings.ADMINS[0][1])
         password = os.environ.get('SUPERUSER_PASS', None)
-        self.stderr.write(f"username: {username}, email: {email}, pw: {password}")
+        self.stdout.write(f"username: {username}, email: {email}, pw: {password}")
         if not User.objects.filter(email=email).exists():
             user = User.objects.create_superuser(username, email, password)
-            self.stderr.write(user)
+            self.stdout.write(user)
         else:
-            self.stderr.write("A user already exists with that email address. ")
+            self.stdout.write("A user already exists with that email address. ")
