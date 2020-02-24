71eb1d1778149660160e23d5dc16c01baee0ce7a
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-23 19:50:58 -0800

    Process checking on createsu command

--- web/hepcat/management/commands/createsu.py
+++ web/hepcat/management/commands/createsu.py
@@ -1,8 +1,7 @@
-from django.core.management.base import BaseCommand  #, CommandError
+from django.core.management.base import BaseCommand  # , CommandError
 # from django.contrib.auth.models import User
 import os
-from django.conf import settings
-# User = settings.AUTH_USER_MODEL
+from django.conf import settings  # User = settings.AUTH_USER_MODEL
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -14,4 +13,7 @@ class Command(BaseCommand):
             username = os.environ.get('SUPERUSER_NAME', settings.ADMINS[0][0])
             email = os.environ.get('SUPERUSER_EMAIL', settings.ADMINS[0][1])
             password = os.environ.get('SUPERUSER_PASS', None)
+            self.stdout.write("================================================")
+            self.stdout.write(f"========== Make SUPERUSER =====================")
+            self.stdout.write(f"username: {username}, email: {email}, pw: {password}")
             User.objects.create_superuser(username, email, password)
