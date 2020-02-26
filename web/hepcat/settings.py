8013d17ca39c2f4ab7e579c4105c7ccea40c3f93
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-26 03:45:57 -0800

    Correct path to storage_backends.py

--- web/hepcat/settings.py
+++ web/hepcat/settings.py
@@ -137,10 +137,10 @@ if USE_S3:
     STATIC_LOCATION = 'www/static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
     # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'hepcat.storage_backends.StaticStorage'
+    STATICFILES_STORAGE = 'web.storage_backends.StaticStorage'
     PUBLIC_MEDIA_LOCATION = 'www/media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'hepcat.storage_backends.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'web.storage_backends.PublicMediaStorage'
 else:
     STATIC_URL = '/static/'
     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
