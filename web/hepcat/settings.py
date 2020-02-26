e956d89927b778f52e3727cf60e023990358ceac
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-26 04:24:09 -0800

    Try custom_url that matches the base_url that was working.

--- web/hepcat/settings.py
+++ web/hepcat/settings.py
@@ -127,20 +127,21 @@ if USE_S3:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-    AWS_DEFAULT_ACL = None  # 'public-read'
+    AWS_DEFAULT_ACL = None
     # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3-website-{AWS_S3_REGION_NAME}.amazonaws.com'
     # ?# AWS_S3_ENDPOINT_URL = AWS_S3_CUSTOM_DOMAIN
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
-    # BASE_S3_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3-website-{AWS_S3_REGION_NAME}.amazonaws.com'
+    BASE_S3_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3-website-{AWS_S3_REGION_NAME}.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = BASE_S3_DOMAIN
     STATIC_LOCATION = 'www/static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATIC_URL = f'https://{BASE_S3_DOMAIN}/{STATIC_LOCATION}/'
     # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'web.storage_backends.StaticStorage'
+    STATICFILES_STORAGE = 'hepcat.storage_backends.StaticStorage'
     PUBLIC_MEDIA_LOCATION = 'www/media'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'web.storage_backends.PublicMediaStorage'
+    MEDIA_URL = f'https://{BASE_S3_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'hepcat.storage_backends.PublicMediaStorage'
 else:
     STATIC_URL = '/static/'
     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
