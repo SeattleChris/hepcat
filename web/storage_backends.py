f8edb5f5a07344c39193c62604891db35225cf1e
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-27 13:06:00 -0800

    Media storage_backend is not used yet, but should not create errors

--- web/storage_backends.py
+++ web/storage_backends.py
@@ -9,7 +9,7 @@ class StaticStorage(S3Boto3Storage):
 
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    # location = settings.MEDIAFILES_LOCATION
     # AWS_S3_CUSTOM_DOMAIN = settings.BASE_S3_DOMAIN
     default_acl = 'public-read'
     file_overwrite = False
