e375d9bc772ce9a4caa111edf66e5ded6f090776
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-26 16:43:48 -0800

    Custom domain set with bucket_name.s3.amaz..., AWS_LOCATION to www, storage_backends set default_acl, and only media has a location value

--- web/storage_backends.py
+++ web/storage_backends.py
@@ -3,13 +3,13 @@ from django.conf import settings
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATIC_LOCATION
-    AWS_S3_CUSTOM_DOMAIN = settings.BASE_S3_DOMAIN
+    # location = settings.STATIC_LOCATION
+    # AWS_S3_CUSTOM_DOMAIN = settings.BASE_S3_DOMAIN
     default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
     location = settings.PUBLIC_MEDIA_LOCATION
-    AWS_S3_CUSTOM_DOMAIN = settings.BASE_S3_DOMAIN
+    # AWS_S3_CUSTOM_DOMAIN = settings.BASE_S3_DOMAIN
     default_acl = 'public-read'
     file_overwrite = False
