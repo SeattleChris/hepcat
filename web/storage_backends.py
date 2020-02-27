9bc189747dd7d8a3964b4c95e3c720f02c3154a9
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-02-26 22:30:25 -0800

    Media and Static settings

--- web/storage_backends.py
+++ web/storage_backends.py
@@ -3,13 +3,13 @@ from django.conf import settings
 
 
 class StaticStorage(S3Boto3Storage):
-    # location = settings.STATIC_LOCATION
+    location = settings.STATICFILES_LOCATION
     # AWS_S3_CUSTOM_DOMAIN = settings.BASE_S3_DOMAIN
     default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = settings.PUBLIC_MEDIA_LOCATION
+    location = settings.MEDIAFILES_LOCATION
     # AWS_S3_CUSTOM_DOMAIN = settings.BASE_S3_DOMAIN
     default_acl = 'public-read'
     file_overwrite = False
