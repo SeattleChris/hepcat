c28d92c4d85738f4826b0a051e21503a1aebad83
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-08-12 19:38:20 -0700

    Tests: Checkin view now covered.

--- tests/views/test_instruction_views.py
+++ tests/views/test_instruction_views.py
@@ -61,22 +61,54 @@ class ClassOfferListViewTests(MimicAsView, TestCase):
         display_date = display_date or self.view.kwargs.get('display_date', None)
         self.test_get_context_data(display_session=display_session, display_date=display_date)
 
-@skip("Not Implemented")
+
 class CheckinListViewTests(MimicAsView, TestCase):
     url_name = 'checkin'
     viewClass = import_string('classwork.views.Checkin')
+    query_order_by = viewClass.query_order_by
 
     def setUp(self):
         self.classoffers = self.setup_three_sessions()
         self.view = self.setup_view('get')
 
-    @skip("Not Implemented")
-    def test_get_queryset(self):
-        pass
+    def test_get_queryset(self, display_session=None, display_date=None):
+        display_session = display_session or self.view.kwargs.get('display_session', None)
+        display_date = display_date or self.view.kwargs.get('display_date', None)
+        self.view.kwargs['display_session'] = display_session
+        self.view.kwargs['display_date'] = display_date
+        view_queryset = self.view.get_queryset()
+        is_ordered = getattr(view_queryset, 'ordered', False)
+        transform = repr
+        sessions = decide_session(sess=display_session, display_date=display_date)
+        model_query = ClassOffer.objects.filter(session__in=sessions)
+        if is_ordered:
+            order = getattr(self, 'query_order_by', [])
+            model_query = model_query.order_by(*order) if order else model_query
+        model_list = [transform(ea) for ea in model_query.all()]
+        expected_list = [transform(ea) for ea in self.classoffers['curr_sess']]
 
-    @skip("Not Implemented")
-    def test_get_context_data(self):
-        pass
+        self.assertQuerysetEqual(view_queryset, expected_list, transform=transform, ordered=False)
+        self.assertQuerysetEqual(view_queryset, model_list, transform=transform, ordered=is_ordered)
+        self.assertSetEqual(set(expected_list), set(model_list))
+        self.assertSetEqual(set(transform(ea) for ea in view_queryset.all()), set(model_list))
+
+    def test_get_context_data(self, display_session=None, display_date=None):
+        self.view.object_list = self.view.get_queryset()  # This step normally done in self.view.get()
+        display_session = display_session or self.view.kwargs.get('display_session', None)
+        display_date = display_date or self.view.kwargs.get('display_date', None)
+        self.view.kwargs['display_session'] = display_session
+        self.view.kwargs['display_date'] = display_date
+        sessions = decide_session(sess=display_session, display_date=display_date)
+        context_sessions = ', '.join([ea.name for ea in sessions])
+        expected_subset = {'sessions': context_sessions}
+        display_session_subset = {'display_session': display_session}  # likely None, unless visited prev or next.
+        display_date_subset = {'display_date': display_date}  # likely None, unless a feature uses this date feature.
+        actual = self.view.get_context_data()
+
+        self.assertDictContainsSubset(expected_subset, actual)
+        self.assertDictContainsSubset(display_session_subset, actual)
+        self.assertDictContainsSubset(display_date_subset, actual)
+
 
 
 @skip("Not Implemented Feature")
