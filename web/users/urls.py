b0daab7df0cd15f2e8a3a4a65f41ff3b478a4ece
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-08-21 23:40:40 -0700

    Form, View, and Url path for custom django_registration.

--- web/users/urls.py
+++ web/users/urls.py
@@ -1,21 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 # from django.views.generic.base import RedirectView
 from django.contrib.auth.views import PasswordChangeView
-from django_registration.backends.one_step.views import RegistrationView
-from .views import SignUp, Modify
-from .forms import CustomUserCreationForm
+from .views import CustomRegistrationView, SignUp, Modify
 # TODO: Report that the UserChangeForm seems to have a hard coded a element href attribute of '../password/'
 
 urlpatterns = [
     # TODO: Set a registration page from one of the following:
-    path('register/',
-         RegistrationView.as_view(
-             success_url='/profile/',
-             form_class=CustomUserCreationForm,
-         ),
-         name='django_registration_register'),
-    # path('user/', include('django_registration.backends.activation.urls')),
-    # path('user/', include('django_registration.backends.one_step.urls')),
+    # path('user/', include('django_registration.backends.activation.urls')),  # for two-step activation workflow
+    path('', include('django_registration.backends.one_step.urls')),
+    path('register/', CustomRegistrationView.as_view(), name='django_registration_register'),
     path('signup/', SignUp.as_view(), name='signup'),
     path('update/', Modify.as_view(), name='user_update'),
     path('password/', PasswordChangeView.as_view(template_name='update.html'), name='password'),
--- web/users/views.py
+++ web/users/views.py
@@ -1,9 +1,16 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import UserHC
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+# from django_registration.views import RegistrationView as BaseRegistrationView
+from django_registration.backends.one_step.views import RegistrationView
+from .forms import CustomRegistrationForm, CustomUserCreationForm, CustomUserChangeForm
 # from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 # Create your views here.
+
+
+class CustomRegistrationView(RegistrationView):
+    form_class = CustomRegistrationForm
+    success_url = reverse_lazy('profile_page')
 
 
 class SignUp(generic.CreateView):
