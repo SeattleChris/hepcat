a506a2fc71d510b28236265384b1dfb8ca325523
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-08-24 23:58:07 -0700

    The 'signup' route now goes to django-registration technique.

--- web/users/urls.py
+++ web/users/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path, include
 # from django.views.generic.base import RedirectView
 from django.contrib.auth.views import PasswordChangeView
+# from django.contrib.auth.views import LoginView
+# from django.contrib.auth.forms import AuthenticationForm
 from .views import CustomRegistrationView, SignUp, Modify
 # TODO: Report that the UserChangeForm seems to have a hard coded a element href attribute of '../password/'
 # TODO: Submit a PR to django-registration for docs update to include One-step workflow URL names.
 
 urlpatterns = [
     # Should have either one-step or two-step workflow setup with the following:
-    path('register/', CustomRegistrationView.as_view(), name='django_registration_register'),  # One-step, customized.
+    path('register/', CustomRegistrationView.as_view(), name='signup'),  # One-step, customized.
     path('', include('django_registration.backends.one_step.urls')),  # One-step, defaults and/or remaining views.
     # path('user/', include('django_registration.backends.activation.urls')),  # for two-step activation workflow
-    path('signup/', SignUp.as_view(), name='signup'),  # Technique not using django-registration.
+    # path('signup/', SignUp.as_view(), name='signup'),  # Technique not using django-registration.
     path('update/', Modify.as_view(), name='user_update'),
     path('password/', PasswordChangeView.as_view(template_name='update.html'), name='password'),
 ]
