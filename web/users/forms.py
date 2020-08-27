71a000ff1b9e0e6ebd7c04af2c97d78b279d65f9
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-08-27 00:47:40 -0700

    Update on strict_email setting.

--- web/users/forms.py
+++ web/users/forms.py
@@ -33,7 +33,7 @@ class CustomRegistrationForm(RegistrationForm):
         email_field_name = self._meta.model.get_email_field_name()
         named_focus = email_field_name if username_field_name in self.data else None
         # TODO: If using RegistrationForm init, then much, but not all, of attach_critical_validators is duplicate code.
-        self.attach_critical_validators(strict_email=bool(named_focus))
+        self.attach_critical_validators()  # strict_email=not bool(named_focus)
         extracted_fields = {key: self.fields.pop(key, None) for key in self.Meta.computed_fields - self.data.keys()}
         self.computed_fields = extracted_fields
         self.focus_correct_field(name=named_focus)
@@ -153,7 +153,7 @@ class CustomRegistrationForm(RegistrationForm):
         self.fields['password1'] = self.fields.pop('password1', None)
         self.fields['password2'] = self.fields.pop('password2', None)
         self.focus_correct_field(name=email_field_name)
-        self.attach_critical_validators(strict_email=True)
+        self.attach_critical_validators()
 
         self.add_error(email_field_name, _("Use a non-shared email, or set a username below. "))
         # self.add_error(flag_name, _("Username only needed if you share an email with another user. "))
